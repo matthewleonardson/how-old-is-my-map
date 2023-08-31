9e210c0b4d80be3f0994d8573f5cba1b572aec17
Matthew Leonardson <matthewcleonardson@gmail.com>
2023-08-31 00:15:53 -0500

    updated about popup

--- src/components/AboutPopup.jsx
+++ src/components/AboutPopup.jsx
@@ -9,7 +9,7 @@ const AboutPopup = ({aboutPopupIsOpen, setAboutPopupIsOpen}) => {
             <div className="aboutPopupInner">
                 <button className="aboutPopupCloseButton" onClick={handleClick}>close</button>
             <h1>About</h1>
-            <p>How Old Is My Map? is a simple website based on XKCD #1668</p>
+            <p>How Old Is My Map? is a simple website based on <a href="https://xkcd.com/1688/">XKCD #1688</a></p>
             </div>
         </div>
     ) : ""
